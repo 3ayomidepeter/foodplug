18948843d71e48c414332448d43524a59f018e04
Ayomide P <112266245+3ayomidepeter@users.noreply.github.com>
2024-12-15 21:53:27 +0100

    Add files via upload

---
+++ foodplug app/foodplug/main.js
@@ -0,0 +1,30 @@
+
+
+
+
+function popsomething() {
+    alert(myname + " I have been coding sincs 12")
+
+}
+
+
+// console.log(document)
+
+var ourbody = document.querySelector(".ourpagebody")
+
+// ourbody.style.background = "black"
+
+var hamburger = document.querySelector(".hamburger-container")
+var mobilelinkscontainer = document.querySelector(".mobile-links-container")
+
+console.log(hamburger)
+
+// hamburger.classList.add("yam")
+
+
+
+function addandremove() {
+    hamburger.classList.toggle("showburger")
+    mobilelinkscontainer.classList.toggle("showmobilelinks")
+}
+
